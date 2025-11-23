f89f1e213159663b8f94826cfc42aba002ec5671
dudow <edu30.nm@gmail.com>
2025-11-23 13:32:14 -0300

    update initial auth page

--- codigo-fonte/src/pages/auth/index.tsx
+++ codigo-fonte/src/pages/auth/index.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import { validateEmail } from "utils/validateEmail";
-import { formatCPF } from "utils/formatCPF";
+import React, { useState } from 'react';
+import { validateEmail } from 'utils/validateEmail';
+import { formatCPF } from 'utils/formatCPF';
 
-import { registerUser, loginUser } from "utils/auth";
-import { useNavigate } from "react-router-dom";
+import { registerUser, loginUser } from 'utils/auth';
+import { useNavigate } from 'react-router-dom';
 
 export default function AuthScreens() {
-  const [mode, setMode] = useState<"signup" | "login">("signup");
+  const [mode, setMode] = useState<'signup' | 'login'>('login');
 
   // CAMPOS
-  const [cpf, setCpf] = useState("");
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [senha, setSenha] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [successMsg, setSuccessMsg] = useState("");
+  const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [senha, setSenha] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
 
   const navigate = useNavigate();
 
@@ -34,9 +34,9 @@ export default function AuthScreens() {
     event.preventDefault();
 
     // ---------- CADASTRO ----------
-    if (mode === "signup") {
+    if (mode === 'signup') {
       if (emailError) {
-        return alert("Corrija os campos destacados antes de continuar.");
+        return alert('Corrija os campos destacados antes de continuar.');
       }
 
       try {
@@ -47,10 +47,10 @@ export default function AuthScreens() {
           password: senha,
         });
 
-        setSuccessMsg("Cadastro realizado com sucesso! Redirecionando...");
+        setSuccessMsg('Cadastro realizado com sucesso! Redirecionando...');
         setTimeout(() => {
-          setMode("login");
-          setSuccessMsg("");
+          setMode('login');
+          setSuccessMsg('');
         }, 1500);
       } catch (err: any) {
         alert(err.message);
@@ -59,13 +59,13 @@ export default function AuthScreens() {
     }
 
     // ---------- LOGIN ----------
-    if (mode === "login") {
+    if (mode === 'login') {
       const user = loginUser(email, senha);
       if (!user) {
-        return alert("Email ou senha incorretos.");
+        return alert('Email ou senha incorretos.');
       }
 
-      navigate("/"); // 🔥 redireciona para Home
+      navigate('/'); // 🔥 redireciona para Home
     }
   };
 
@@ -75,7 +75,7 @@ export default function AuthScreens() {
         <div className="pointer-events-none absolute inset-0 rounded-[28px] ring-4 ring-[#0ea5e9] -z-10" />
 
         <h1 className="text-4xl font-extrabold text-black text-center mb-8">
-          {mode === "signup" ? "Cadastro" : "Login"}
+          {mode === 'signup' ? 'Cadastro' : 'Login'}
         </h1>
 
         {/* Sucesso */}
@@ -87,7 +87,7 @@ export default function AuthScreens() {
 
         <form onSubmit={handleSubmit}>
           {/* CAMPOS ESPECÍFICOS DO CADASTRO */}
-          {mode === "signup" && (
+          {mode === 'signup' && (
             <>
               <input
                 name="cpf"
@@ -119,14 +119,16 @@ export default function AuthScreens() {
             placeholder="Seu e-mail"
             className={`w-full mb-2 px-5 py-3 rounded-full border-2 ${
               emailError
-                ? "border-red-500 focus:ring-red-400"
-                : "border-[#3b0a8a] focus:border-[#5b21b6]"
+                ? 'border-red-500 focus:ring-red-400'
+                : 'border-[#3b0a8a] focus:border-[#5b21b6]'
             } placeholder:text-[#bcbcd2] outline-none focus:ring-2`}
             required
           />
 
           {emailError && (
-            <p className="text-red-500 text-sm mb-3 text-center">{emailError}</p>
+            <p className="text-red-500 text-sm mb-3 text-center">
+              {emailError}
+            </p>
           )}
 
           {/* SENHA */}
@@ -141,7 +143,7 @@ export default function AuthScreens() {
           />
 
           {/* Termos / Mantenha-me logado */}
-          {mode === "signup" ? (
+          {mode === 'signup' ? (
             <label className="flex items-start gap-3 text-sm text-black/80 select-none mb-5">
               <input
                 type="checkbox"
@@ -168,16 +170,16 @@ export default function AuthScreens() {
             disabled={!!successMsg}
             className="w-full h-12 rounded-full bg-[#ffd000] hover:bg-[#ffcd00]/90 transition font-semibold text-black"
           >
-            {mode === "signup" ? "Cadastre-se" : "Acesse sua conta"}
+            {mode === 'signup' ? 'Cadastre-se' : 'Acesse sua conta'}
           </button>
 
           <p className="text-center text-sm text-gray-500 mt-6">
-            {mode === "signup" ? (
+            {mode === 'signup' ? (
               <>
-                Já possui conta?{" "}
+                Já possui conta?{' '}
                 <button
                   type="button"
-                  onClick={() => setMode("login")}
+                  onClick={() => setMode('login')}
                   className="text-[#ef476f] underline"
                 >
                   Fazer login
@@ -185,10 +187,10 @@ export default function AuthScreens() {
               </>
             ) : (
               <>
-                Não tem uma conta?{" "}
+                Não tem uma conta?{' '}
                 <button
                   type="button"
-                  onClick={() => setMode("signup")}
+                  onClick={() => setMode('signup')}
                   className="text-[#ef476f] underline"
                 >
                   Cadastre-se
